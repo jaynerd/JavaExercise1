42568e43a749f1a1e6e90287f19a52daff88d975
unknown <j4yn3rd@gmail.com>
2018-06-16 03:12:13 +1200

    variable assigning exercise added

---
+++ src/variable/VariableExercise.java
@@ -0,0 +1,15 @@
+package variable;
+
+/**
+ * This simple variable assigning exercise demonstrates how variables
+ * can be assigned, and perform a print out function to the console to
+ * check the stored value of the variable.
+ */
+
+public class VariableExercise {
+    public static void main(String[] args) {
+        int sampleValue = 53;
+        System.out.println("The value of sampleValue variable is : " +
+                sampleValue + ".");
+    }
+}
